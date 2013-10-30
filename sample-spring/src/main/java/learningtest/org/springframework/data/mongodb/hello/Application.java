5f6f31c7f69626924e7074338481a5938bad8733
izeye <izeye@naver.com>
2013-10-30 16:45:22 +0900

    Add a Spring Data MongoDB sample.

---
+++ sample-spring/src/main/java/learningtest/org/springframework/data/mongodb/hello/Application.java
@@ -0,0 +1,63 @@
+package learningtest.org.springframework.data.mongodb.hello;
+
+import java.net.UnknownHostException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.Mongo;
+
+@Configuration
+@EnableMongoRepositories
+public class Application {
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Bean
+	Mongo mongo() throws UnknownHostException {
+		return new Mongo();
+	}
+
+	@Bean
+	MongoTemplate mongoTemplate(Mongo mongo) {
+		return new MongoTemplate(mongo, "gs-accessing-data-mongo");
+	}
+
+	public static void main(String[] args) {
+		AbstractApplicationContext context = new AnnotationConfigApplicationContext(
+				Application.class);
+		CustomerRepository repository = context
+				.getBean(CustomerRepository.class);
+
+		repository.deleteAll();
+
+		repository.save(new Customer("Alice", "Smith"));
+		repository.save(new Customer("Bob", "Smith"));
+
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("---------------------------------------------");
+		System.out.println(repository.findByFirstName("Alice"));
+
+		System.out.println("Customer found with findByLastName('Smith'):");
+		System.out.println("--------------------------------------------");
+		for (Customer customer : repository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
+
+		context.close();
+	}
+
+}
